06b953c771cf0cbc8d819d835420091806934d0b
unknown <miltonsergiom@gmail.com>
2024-11-03 19:56:56 -0400

    feat: api node with postgres

---
+++ db.js
@@ -0,0 +1,7 @@
+import 'dotenv/config'
+import { neon } from "@neondatabase/serverless";
+
+export const sql = neon(process.env.DATABASE_URL);
+
+
+
---
+++ server.js
@@ -0,0 +1,49 @@
+import { fastify } from 'fastify';
+// import { DatabaseMemory } from './database-memory.js';
+import { DatabasePostgres } from './database-postgres.js';
+
+const server = fastify();
+
+// const database = new DatabaseMemory();
+
+const database = new DatabasePostgres();
+
+server.post('/videos', async (request, reply) => {
+    
+    await database.create(request.body);
+
+    return reply.status(201).send()
+
+});
+
+server.get('/videos', async (request) => {
+    const search = request.query.search;
+
+
+    const videos = await database.list(search);
+
+    return videos
+    
+});
+
+server.put('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id;
+
+    await database.update(videoId, request.body);
+    
+
+    return reply.status(204).send()
+});
+
+server.delete('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id;
+
+    await database.delete(videoId);
+
+    return reply.status(204).send()
+      
+  });
+
+server.listen({
+    port: process.env.PORT ?? 3333,
+})
